830929a9bcc8b4d08438e44c85a2125c4cc8c3dd
deepanshukh0411 <deepanshukh0411@gmail.com>
2026-04-25 17:10:56 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -38,6 +38,7 @@ if ingredients_list:
     ingredients_string=""
     for i in ingredients_list:
         ingredients_string+=i+" " 
+        st.write(pd_df.columns)
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(i+" Nutrition Information ")
