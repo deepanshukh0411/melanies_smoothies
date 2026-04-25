9cd16c1567d95eadbfbf3934010701d5b24470a5
deepanshukh0411 <deepanshukh0411@gmail.com>
2026-04-25 16:13:20 +0530

    Update steamlit_app.py
    because of moving to Snis from SIS

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -23,8 +22,8 @@ st.write('The name on Smoothie will be:',name_on_order)
 # option=st.selectbox('What is your favorite fruit?',
 # ('Banana','Strawberries','Peaches'))
 # st.write('You favorite fruit is:',option)
-
-session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list=st.multiselect('Choose upto 5 ingredients:',my_dataframe,max_selections=5)
