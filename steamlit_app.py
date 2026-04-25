e03de165b2ff54cfde52c76a14a690a96eecc399
deepanshukh0411 <deepanshukh0411@gmail.com>
2026-04-25 16:49:49 +0530

    Fix formatting issue in API request line

--- steamlit_app.py
+++ steamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
     for i in ingredients_list:
         ingredients_string+=i+" " 
         st.subheader(i+" Nutrition Information ")
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     st.write(ingredients_string)
