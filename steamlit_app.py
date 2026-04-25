218f14d10b72bda0e1fcf48ffa4b336fd622a9d6
deepanshukh0411 <deepanshukh0411@gmail.com>
2026-04-25 17:16:27 +0530

    Update search value display for selected fruit

--- steamlit_app.py
+++ steamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         ingredients_string+=i+" " 
         #st.write(pd_df.columns)
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', i,' is ', search_on, '.')
         st.subheader(i+" Nutrition Information ")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
