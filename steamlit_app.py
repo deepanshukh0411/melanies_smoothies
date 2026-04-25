b7118ccd9a0ebcdd07746539b232fea71273df96
deepanshukh0411 <deepanshukh0411@gmail.com>
2026-04-25 16:45:49 +0530

    Refactor smoothie order logic by removing unused API call
    Removed redundant API call and dataframe display for smoothie ingredients.

--- steamlit_app.py
+++ steamlit_app.py
@@ -34,6 +34,9 @@ if ingredients_list:
     ingredients_string=""
     for i in ingredients_list:
         ingredients_string+=i+" "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
     st.write(ingredients_string)
     
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
@@ -44,8 +47,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+
 #st.text(smoothiefroot_response)
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
